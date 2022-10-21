28664d775328e8605e8791299c34fcfba95eee72
David Tucker <davidt2003@msn.com>
2022-10-21 08:11:21 -0400

    feat: ability to view eth balance

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 let express = require('express');
 let router = express.Router();
 
+const Web3 = require("web3");
+const EthereumTx = require('ethereumjs-tx').Transaction;
+const axios = require('axios');
+const ethNetwork = 'https://goerli.infura.io/v3/914cc489153047218ff33a71beab2a8f';
+const web3 = new Web3(new Web3.providers.HttpProvider(ethNetwork));
+
 const publicAddress = "0xEcc179a1b25Dad72D022CD830F9d26221fcA8A81";
 
-router.get('/', function(req, res) {
+router.get('/', async function(req, res) {
     res.render('index', {
-        balance: getBalance(publicAddress),
+        balance: await getBalance(publicAddress),
         error: req.flash('error'),
         success: req.flash('success'),
         address: publicAddress
@@ -42,9 +48,16 @@ router.post('/', async function (req, res) {
     res.redirect("/");
 });
 
-function getBalance(address) {
-    // TODO: Retrieve the real ETH balance for a given address
-    return parseFloat("0").toFixed(8);
+async function getBalance(address) {
+    return new Promise((resolve, reject) => {
+        web3.eth.getBalance(address, async (err, result) => {
+            if (err) {
+                return reject(err);
+            }
+            const eth = web3.utils.fromWei(result, "ether");
+            resolve(parseFloat(eth).toFixed(5));
+        });
+    });
 }
 
 function sendEthereum(toAddress, ethAmount) {
